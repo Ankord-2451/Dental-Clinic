f2ffa8ef7d2a41d7572e7811498751b99619a2a4
Ankord-2451 <107429227+Ankord-2451@users.noreply.github.com>
2023-01-10 21:00:55 +0200

    added controller and view for authorization

---
+++ Dental Clinic/Controllers/AuthorizationController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Dental_Clinic.Controllers
+{
+    public class AuthorizationController : Controller
+    {
+        [HttpGet("Authorization/Form")]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost("Authorization/Form")]
+        public ActionResult Index(string login,string password)
+        {
+            return View();
+        }
+    }
+}
