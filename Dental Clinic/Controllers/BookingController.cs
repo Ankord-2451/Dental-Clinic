fe1983137bb9e34bdfd82079a8382f95ff52d30c
Ankord-2451 <107429227+Ankord-2451@users.noreply.github.com>
2023-01-15 18:15:03 +0200

    Create controller and view for Booking Page

---
+++ Dental Clinic/Controllers/BookingController.cs
@@ -0,0 +1,24 @@
+﻿using Dental_Clinic.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Dental_Clinic.Controllers
+{
+    public class BookingController : Controller
+    {
+        [HttpGet("Booking")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost("Booking")]
+        public IActionResult Index(EmployeeModel employee)
+        {
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
