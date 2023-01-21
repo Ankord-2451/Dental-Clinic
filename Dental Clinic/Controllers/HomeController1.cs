1db0e562005f8c00bf65eb945cb8c9c675147fee
Ankord-2451 <107429227+Ankord-2451@users.noreply.github.com>
2023-01-21 12:06:59 +0200

    tested authorization and authentication

--- Dental Clinic/Controllers/HomeController1.cs
+++ Dental Clinic/Controllers/HomeController1.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using Dental_Clinic.Core;
+using Dental_Clinic.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
@@ -15,7 +17,13 @@ namespace Dental_Clinic.Controllers
         [HttpGet("/jwt")]
         public ActionResult View1()
         {
-            return View();
+            var session = new SessionWorker(HttpContext);
+            var user = new AuthUserModel()
+            {
+                ID = session.GetUserId(),
+                name = session.GetUserName()
+            };
+            return View(user);
         }
 
         
