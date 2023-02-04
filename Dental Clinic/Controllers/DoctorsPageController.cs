d48ad721efe4f4e9a27b7f13af02cd83bbcbad74
Ankord-2451 <107429227+Ankord-2451@users.noreply.github.com>
2023-02-04 09:46:48 +0200

    Add Page For Doctors

---
+++ Dental Clinic/Controllers/DoctorsPageController.cs
@@ -0,0 +1,101 @@
+﻿using Dental_Clinic.Core;
+using Dental_Clinic.Data;
+using Dental_Clinic.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Dental_Clinic.Controllers
+{
+    [Authorize]
+    public class DoctorsPageController : Controller
+    {
+
+        private ApplicationDbContext dbContext;
+
+        public DoctorsPageController(ApplicationDbContext _dbContext)
+        {
+            dbContext = _dbContext;
+        }
+
+
+        [HttpGet("DoctorsPage")]
+        public ActionResult Index()
+        {
+           
+            var sessionWorker = new SessionWorker(HttpContext);
+            
+            string NameOfDoctor = sessionWorker.GetUserName();
+
+            List<EntryFormModel> records = dbContext.ListOfRecords.
+                Where(x=>x.Doctor==NameOfDoctor).
+                      OrderByDescending(x => x.ID).ToList();
+           
+            return View(records);
+        }
+
+
+
+        [HttpGet("DoctorsPage/Details/{id?}")]
+        public ActionResult Details(int id)
+        {
+            EntryFormModel record = dbContext.ListOfRecords.First(x => x.ID == id);
+
+            return View(record);
+        }
+
+
+        [HttpGet("DoctorsPage/Edit/{id?}")]
+        public ActionResult Edit(int id,string Time_problem_message)
+        {
+            ViewData["ListOfDoctors"] = dbContext.employees.Where(e => e.Role == role.Doctor).ToList();
+
+            ViewData["ListOfProcedures"] = dbContext.ListOfProcedure.ToList();
+
+            ViewData["Time_problem_message"] = Time_problem_message;
+
+            EntryFormModel record = dbContext.ListOfRecords.First(x => x.ID == id);
+
+            return View(record);
+        }
+
+        [HttpPost("DoctorsPage/Edit/{id?}")]
+        public ActionResult Edit(int id, EntryFormModel record)
+        {
+            string Time_problem_message = null;
+
+            if (ModelState.IsValid)
+            {
+
+                ProcedureModel procedure = dbContext.ListOfProcedure.First(e => e.Name == record.Procedure);
+                Time_problem_message = CheckTimeHelper.Check(
+                  record,
+                  procedure.NeedHoursOnProcedure,
+                  procedure.NeedMinutesOnProcedure,
+                  dbContext.ListOfRecords.ToList()
+                      );
+
+
+                if (Time_problem_message == null)
+                {
+                    dbContext.ListOfRecords.Add(record);
+                    dbContext.SaveChanges();
+                    return Index();
+                }
+            }
+            return Edit(record.ID,Time_problem_message);
+        }
+
+
+
+        [HttpPost("DoctorsPage/Delete/{id?}")]
+        public ActionResult Delete(int id)
+        {
+            EntryFormModel record = dbContext.ListOfRecords.First(x => x.ID == id);
+            dbContext.ListOfRecords.Remove(record);
+            dbContext.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
