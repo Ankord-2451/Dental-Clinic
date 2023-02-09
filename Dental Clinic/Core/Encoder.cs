3660bef3a622a53fc3d808e6ce7b140cefd4a780
Ankord-2451 <107429227+Ankord-2451@users.noreply.github.com>
2023-02-09 20:25:38 +0200

    Started work with passwords

---
+++ Dental Clinic/Core/Encoder.cs
@@ -0,0 +1,21 @@
+﻿
+
+using System.Security.Cryptography;
+using System;
+using System.Text;
+using Dental_Clinic.Models;
+
+namespace Dental_Clinic.Core
+{
+    public static class Encoder
+    {
+       public static void Encode(EmployeeModel employee)
+        { 
+       // HMACSHA256 hm = new HMACSHA256(Encoding.UTF8.GetBytes(employee.Password));
+       // byte[] result = hm.ComputeHash(enc.GetBytes(packet));
+       // String hex = BitConverter.ToString(result);
+       // hex = hex.Replace("-", "");
+   // Console.WriteLine(hex.ToLower());
+        }
+    }
+}
